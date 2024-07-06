d87f4e7dc0c11139f5c773ec42655c6e2b6a8c63
Ananddivine <109467220+Ananddivine@users.noreply.github.com>
2024-07-07 03:40:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,15 +35,20 @@ app.get('/files', (req, res) => {
     }
 
     const filesWithDescriptions = files.filter(file => !file.endsWith('_reply.txt')).map(file => {
-      const descriptionPath = path.join('public/upload/', `${file}.txt`);
-      const description = fs.existsSync(descriptionPath) ? fs.readFileSync(descriptionPath, 'utf8') : '';
-      const fileUrl = `${req.protocol}://${req.get('host')}/upload/${file}`;
-      return { filename: file, description, url: fileUrl };
-    });
+      const metadataPath = path.join('public/upload/', `${file}.json`);
+      if (fs.existsSync(metadataPath)) {
+        const metadata = JSON.parse(fs.readFileSync(metadataPath, 'utf8'));
+        const fileUrl = `${req.protocol}://${req.get('host')}/upload/${file}`;
+        return { filename: file, ...metadata, url: fileUrl };
+      } else {
+        return null;
+      }
+    }).filter(file => file !== null);
 
     res.json(filesWithDescriptions);
   });
 });
+
 
 const upload = multer({ storage });
 
@@ -52,18 +57,20 @@ app.post('/upload', upload.single('uploadedFile'), (req, res) => {
     return res.status(400).send('No file uploaded.');
   }
 
-  const description = req.body.description || 'No description';
-  console.log('Request body:', req.body.description);
+  const { description, username, email } = req.body;
 
   const filename = req.file.originalname;
 
+  // Move the uploaded file to the desired location
   fs.renameSync(req.file.path, path.join('public/upload/', filename));
 
-  fs.writeFileSync(path.join('public/upload/', `${filename}.txt`), description);
-  console.log('request description:', path.join('public/upload/', `${filename}.txt`), description);
+  // Save the description and metadata
+  const metadata = { description: description || 'No description', username, email };
+  fs.writeFileSync(path.join('public/upload/', `${filename}.json`), JSON.stringify(metadata));
 
   res.send('File uploaded successfully.');
 });
+
 
 app.post('/files/:filename/replies', (req, res) => {
   let filename = req.params.filename;
