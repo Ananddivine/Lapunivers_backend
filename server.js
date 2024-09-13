1a0173a172d25dbfb3697f74bece3b37536be098
Ananddivine <109467220+Ananddivine@users.noreply.github.com>
2024-09-13 23:36:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,21 +4,65 @@ const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors({
-  origin: 'https://lapunivers.vercel.app',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true
-}));
+// CORS configuration
+const allowedOrigins = [
+  "https://lapunivers.vercel.app"
+  ];
 
-app.use(express.static('public'));
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'auth-token'],
+  credentials: true,
+  optionsSuccessStatus: 200
+}));app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://ad91482948:zqEgXCJrQF4cONgk@cluster0.udg8e.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(async () => {
+  console.log("Connected to MongoDB");
+
+  // Drop the index if it exists
+  try {
+    await mongoose.connection.db.collection('users').dropIndex('image_1');
+    console.log("Index 'image_1' dropped successfully");
+  } catch (err) {
+    if (err.codeName === 'IndexNotFound') {
+      console.log("Index 'image_1' not found");
+    } else {
+      console.log("Error dropping index:", err.message);
+    }
+  }
+
+}).catch(err => {
+  console.log("Error connecting to MongoDB:", err.message);
+});
+
+
+  // API creation
+  app.use('/images', express.static('public/upload'));
+  app.get("/", (req, res) => {
+    res.send("Express App is Running");
+  });
 // Ensure upload directory exists
 const uploadDir = path.join(__dirname, 'public', 'upload');
 if (!fs.existsSync(uploadDir)) {
@@ -31,13 +75,150 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: function (_req, file, cb) {
-    cb(null, file.originalname);
+    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+  }
+
+});
+const upload = multer({ storage });
+
+
+
+
+  // Creating upload image for adminpanel
+  app.post("/upload", upload.single('product'), (req, res) => {
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        message: "No file uploaded"
+      });
+    }
+
+    res.json({
+      success: 1,
+      Image_url:`https://lapuniversbackend-production.up.railway.app/images/${req.file.filename}`
+    });
+  });
+// Schema for creating product
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+
+
+// Creating API for deleting the product
+app.post('/removeproduct', async (req, res) => {
+  try {
+    const product = await Product.findOneAndDelete({ id: req.body.id });
+    if (!product) {
+      res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+      return;
+    }
+
+    console.log("Removed:", product);
+    res.json({
+      success: true,
+      name: req.body.name,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
   }
 });
 
+// Creating API for getting all products
+app.get('/allproducts', async (req, res) => {
+  let products = await Product.find({});
+  console.log("All products fetched");
+  res.send(products);
+});
+
+// Schema for creating user model
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cartData: {
+    type: Object,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+
+// Creating end point for users uploads
+app.post('/uploadfile', upload.single('uploadedFile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const { description, username, token } = req.body;
+
+  const filename = req.file.originalname;
+
+  // Move the uploaded file to the desired location
+  fs.renameSync(req.file.path, path.join('public/upload/', filename));
+
+  // Save the description and metadata
+  const metadata = { description: description || 'No description', username, token };
+  fs.writeFileSync(path.join('public/upload/', `${filename}.json`), JSON.stringify(metadata));
+
+  res.send('File uploaded successfully.');
+});
+
 // File handling routes...
-
-
 app.get('/files', (req, res) => {
   fs.readdir('public/upload/', (err, files) => {
     if (err) {
@@ -61,26 +242,8 @@ app.get('/files', (req, res) => {
 });
 
 
-const upload = multer({ storage });
 
-app.post('/upload', upload.single('uploadedFile'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).send('No file uploaded.');
-  }
 
-  const { description, username, email } = req.body;
-
-  const filename = req.file.originalname;
-
-  // Move the uploaded file to the desired location
-  fs.renameSync(req.file.path, path.join('public/upload/', filename));
-
-  // Save the description and metadata
-  const metadata = { description: description || 'No description', username, email };
-  fs.writeFileSync(path.join('public/upload/', `${filename}.json`), JSON.stringify(metadata));
-
-  res.send('File uploaded successfully.');
-});
 
 
 app.post('/files/:filename/replies', (req, res) => {
@@ -106,23 +269,31 @@ app.post('/files/:filename/replies', (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
-
 app.put('/files/:filename/replies', (req, res) => {
   let filename = req.params.filename;
+  
+  // Remove ".txt" extension from filename if present
   if (filename.endsWith('.txt')) {
     filename = filename.slice(0, -4);
   }
+
   const replyFilePath = path.join('public/upload/', `${filename}_reply.txt`);
   const updatedReply = req.body.updatedReply;
+  const username = req.body.username; // Capture username from the request body
 
   try {
-    fs.writeFileSync(replyFilePath, `${updatedReply}\n`);
+    // Append the updated reply along with the username
+    const replyContent = `${username}: ${updatedReply}\n`;
+    
+    // Overwrite the reply file with the new content
+    fs.writeFileSync(replyFilePath, replyContent);
     res.send('Reply updated successfully.');
   } catch (error) {
     console.error('Error updating reply file:', error);
     res.status(500).send('Internal Server Error');
   }
 });
+
 
 app.get('/files/:filename/replies', (req, res) => {
   let filename = req.params.filename;
@@ -206,6 +377,292 @@ app.delete('/files/:filename/replies', (req, res) => {
     res.send('Reply file deleted successfully.');
   });
 });
+
+app.post('/addproduct', async (req, res) => {
+  try {
+    let products = await Product.find({});
+    let id;
+
+    if (products.length > 0) {
+      let last_product = products[products.length - 1];
+      id = last_product.id + 1;
+    } else {
+      id = 1;
+    }
+
+    const product = new Product({
+      id: id,
+      name: req.body.name,
+      image: req.body.image,
+      category: req.body.category,
+      new_price: req.body.new_price,
+      old_price: req.body.old_price,
+      description: req.body.description,
+    });
+
+    console.log(product);
+    await product.save();
+    console.log("saved");
+
+    res.json({
+      success: true,
+      name: req.body.name,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+app.post('/signup', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Check if email already exists
+    let existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ success: false, error: "Existing User Found" });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Initialize cartData
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+      cart[i] = 0;
+    }
+
+    // Generate unique token 
+    const token = crypto.randomBytes(20).toString('hex');
+
+    // Create user
+    const user = new User({
+      name: username,
+      email,
+      password: hashedPassword,
+      cartData: cart,
+      token: token,
+    });
+
+    await user.save();
+
+    // Generate JWT token
+    const data = {
+      user: {
+        id: user.id,
+        name: user.name
+      }
+    };
+    const authToken = jwt.sign(data, 'anand_secret');
+    res.json({ success: true,  authToken, token: user.token,  name: user.name });
+
+    // Set up email sending
+    const transporter = nodemailer.createTransport({
+      service: 'ad91482948@gmail.com',
+      auth: {
+        user: 'ad91482948@gmail.com',
+        pass: 'apux vtxh copq iugq',
+      },
+    });
+
+    const mailOptions = {
+      from: 'ad91482948@gmail.com',
+      to: email,
+      subject: 'Thank you for registering',
+      text: 'Thank you for registering on our platform!',
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Email sending error:", error);
+        return res.status(500).json({ success: false, message: 'Error sending email' });
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.status(201).json({ success: true, token, name: user.name });
+      }
+    });
+
+  } catch (error) {
+    console.error("Error during registration:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ success: false, error: 'User not found' });
+    }
+
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ success: false, error: 'Wrong password' });
+    }
+
+    // Generate JWT token
+    const data = {
+      user: {
+        id: user._id,
+        name: user.name
+      }
+    };
+
+    const authToken = jwt.sign(data, 'anand_secret');
+    res.json({ success: true,  authToken, token: user.token,  name: user.name });
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
+
+
+
+
+
+
+// Route to send OTP
+app.post('/forgot-password', async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    // Find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Generate OTP
+    const otp = crypto.randomInt(100000, 999999).toString();
+
+    // Set OTP and expiration time (e.g., 10 minutes)
+    user.otp = otp;
+    user.otpExpiration = Date.now() + 10 * 60 * 1000; // 10 minutes
+    await user.save();
+
+    // Send OTP via email
+    const transporter = nodemailer.createTransport({
+      service: 'ad91482948@gmail.com', // Use your email service
+      auth: {
+        user: 'ad91482948@gmail.com',
+        pass: 'apux vtxh copq iugq',
+      },
+    });
+
+    const mailOptions = {
+      from: 'ad91482948@gmail.com',
+      to: email,
+      subject: 'Password Reset OTP',
+      text: `Your OTP for password reset is ${otp}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).send('Error sending OTP');
+      } else {
+        console.log('OTP email sent: ' + info.response);
+        res.status(200).send('OTP sent to email');
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error sending OTP');
+  }
+});
+// Route to verify OTP
+app.post('/verify-otp', async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+
+    // Find user by email
+    const user = await User.findOne({ email });
+    if (!user || user.otp !== otp || Date.now() > user.otpExpiration) {
+      return res.status(400).send('Invalid or expired OTP');
+    }
+
+    // Clear OTP after verification
+    user.otp = undefined;
+    user.otpExpiration = undefined;
+    await user.save();
+
+    res.status(200).send('OTP verified');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error verifying OTP');
+  }
+});
+
+app.post('/reset-password', async (req, res) => {
+  try {
+    const { email, otp, newPassword } = req.body;
+
+    // Find user by email and check OTP and expiration
+    const user = await User.findOne({ email, otp });
+    if (!user || Date.now() > user.otpExpiration) {
+      return res.status(400).send('Invalid or expired OTP');
+    }
+
+    // Hash new password
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    // Update password and clear OTP
+    user.password = hashedPassword;
+    user.otp = undefined;
+    user.otpExpiration = undefined;
+    await user.save();
+
+    // Send confirmation email
+    const transporter = nodemailer.createTransport({
+      service: 'gmail', // Specify email service provider
+      auth: {
+        user: 'ad91482948@gmail.com',
+        pass: 'apux vtxh copq iugq',
+      },
+    });
+
+    const mailOptions = {
+      from: 'ad91482948@gmail.com',
+      to: email,
+      subject: 'Your LapUniverse Password Has Been Reset',
+      text: `Dear User,
+
+Your LapUniverse account password has been successfully reset. If you did not perform this action, please report this immediately by contacting us at the following link:
+
+https://lapunivers.vercel.app/Contact
+
+Thank you,
+LapUniverse Security Team`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending confirmation email:', error);
+      } else {
+        console.log('Confirmation email sent:', info.response);
+      }
+    });
+
+    res.status(200).send('Password reset successful');
+  } catch (error) {
+    console.error('Error resetting password:', error);
+    res.status(500).send('Error resetting password');
+  }
+});
+
+
+
+
+
+
 
 
 
